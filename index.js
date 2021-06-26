26fea9e28876ad8a892134b77bbcc1ad23dd8d7c
InimicalPart <37552123+InimicalPart@users.noreply.github.com>
2021-06-26 16:02:33 +0200

    Adding .approve, .deny and .restore

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const Discord = require("discord.js");
 require("dotenv").config();
 const prefix = process.env.prefix
 const token = process.env.NotMyToken
+const submissionChannelID = "858140842798743603"
+const submissionQueueID = "858140842798743603"
 if (token == null) {
 	console.log("Token is missing, please make sure you have the .env file in the directory with the correct information. Please see https://github.com/InimicalPArt/TheIIIProject for more information.")
 	process.exit(1)
 } else if (prefix == null) {
 	console.log("Prefix is missing, The consequences of this will be that the bot wont have a prefix and will react to messages like 'test'. please make sure you have the .env file in the directory with the correct information. Please see https://github.com/InimicalPart/TheIIIProject for more information.")
 }
-const botOwners = ["ray.#2021", "InimicalPart ©#0001", "lethal.#0111"]
+const botOwners = ["745783548241248286", "301062520679170066", "426826826220961821"]
 function attachIsImage(msgAttach) {
 	var url = msgAttach.url;
 	//True if this url is a png image.
@@ -53,11 +55,20 @@ function between(min, max) {
 		Math.random() * (max - min + 1) + min
 	)
 }
+async function findMessage(message, ID) {
+	let channels = message.guild.channels.cache.filter(c => c.type == 'text').array();
+	for (let current of channels) {
+		let target = await current.messages.fetch(ID);
+		if (target) { console.log(target); return target; }
+	}
+}
+
 client.on('message', async (message) => {
 	if (message.author.bot) return;
-	const args = message.content.split(' ').shift();
+	let args = []
+	args = message.content.split(" ").slice(1);//.split(" ");
 	if (message.content.startsWith(prefix + "test")) {
-		if (botOwners.includes(message.author.tag)) {
+		if (botOwners.includes(message.author.id)) {
 			message.channel.send("Hi! My brain tells me that you are one of my owners :)")
 		} else {
 			message.channel.send("Hmm... You're not my owner! >:(")
@@ -111,7 +122,7 @@ client.on('message', async (message) => {
 								return message.channel.send("This title is too long! Try again with a shorter title");
 							} else {
 								title = messageNext.content;
-								const submissionChannel = client.channels.cache.get("858140842798743603");
+								const submissionChannel = client.channels.cache.get(submissionChannelID);
 								/*submissionChannel.send("**" + title + "**");
 								submissionChannel.send(url);
 								submissionChannel.send("Amazing picture by: <@" + message.author + ">")
@@ -143,8 +154,211 @@ client.on('message', async (message) => {
 			}
 		});
 
+	} else if (message.content.startsWith(prefix + "remove")) {
+		if (!botOwners.includes(message.author.id)) return message.channel.send("Hmm... You don't seem to have enough permissions to do that.")
+
+		if (!args[0]) {
+			return message.channel.send("Please provide the message to remove")
+		}
+		const submissionChannel = client.channels.cache.get(submissionChannelID);
+		const messageID = args[0];
+		let m = await submissionChannel.messages.fetch(messageID).catch((err) => {
+			return message.channel.send("Invalid Message ID.")
+		})
+		if (m == undefined) {
+			return message.channel.send("Invalid Message ID.");
+		}
+
+
+		//return message.channel.send(messageID + "s content is: " + m.content)
+		m.delete().then(() => message.channel.send("Successfully deleted " + messageID)).catch((err) => message.channel.send("ERROR: " + err));
+
+	} else if (message.content.startsWith(prefix + "deny")) {
+		if (!botOwners.includes(message.author.id)) return //message.channel.send("Hmm... You don't seem to have enough permissions to do that.")
+		message.delete();
+		if (!args[0]) {
+			return message.channel.send("Please provide the message to deny")
+		}
+		if (!args[1]) {
+			return message.channel.send("Please provide the reason for the deny")
+		}
+		const submissionChannel = client.channels.cache.get(submissionChannelID);
+		const messageID = args[0];
+		const reason = args.slice(1).join(" ");
+		let m = await submissionChannel.messages.fetch(messageID).catch((err) => {
+			return message.channel.send("Invalid Message ID.")
+		})
+		if (m == undefined) {
+			return message.channel.send("Invalid Message ID.");
+		}
+
+
+		//return message.channel.send(messageID + "s content is: " + m.content)
+		if (!m.editable) return message.channel.send("I cannot edit that message.")
+		if (m.embeds.length < 1) return message.channel.send("This isn't an image submission.")
+		const url = m.embeds[0].image.url
+		const authorID = m.embeds[0].author.iconURL.match(/[0-9]{18}/gmi)
+		let title = null;
+		for (let field of m.embeds[0].fields) {
+			if (field.name == 'Title:') {
+				title = field.value;
+				break
+			}
+		}
+		if (title == null) {
+			return message.channel.send("This isn't an image submission.")
+		}
+		const author = await client.users.fetch(String(authorID));
+
+		//console.log(url, authorID, title, author.tag)
+		let exists = null;
+		for (let field of m.embeds[0].fields) {
+			if (field.name == 'Information:') {
+				exists = field.value;
+				break
+			}
+		}
+		//console.log(exists)
+		if (exists != null) {
+			return message.channel.send("This message has already been denied or approved.")
+		}
+		const newEmbed = new Discord.MessageEmbed()
+			.setAuthor(author.tag, author.avatarURL())
+			.setImage(url)
+			.setColor("#FFFF00")
+			.addField("Title:", "**" + title + "**")
+			.addField("Amazing picture by:", "<@" + message.author + ">")
+			.addField('\u200b', '\u200b')
+			.addField('Information:', ':x: | Denied by Moderator: ' + reason);
+		m.edit(newEmbed)
+		await author.send("Your image submission was denied: " + reason).then(() => {
+			message.channel.send("DM Sent.")
+		}).catch(() => {
+			message.channel.send("User has DMs closed / No mutual servers");
+		});
+	} else if (message.content.startsWith(prefix + "approve")) {
+		if (!botOwners.includes(message.author.id)) return// message.channel.send("Hmm... You don't seem to have enough permissions to do that.")
+		message.delete();
+		if (!args[0]) {
+			return message.channel.send("Please provide the message to approval")
+		}
+		if (!args[1]) {
+			return message.channel.send("Please provide the reason for the approval")
+		}
+		const submissionChannel = client.channels.cache.get(submissionChannelID);
+		const messageID = args[0];
+		const reason = args.slice(1).join(" ");
+		let m = await submissionChannel.messages.fetch(messageID).catch((err) => {
+			return message.channel.send("Invalid Message ID.")
+		})
+		if (m == undefined) {
+			return message.channel.send("Invalid Message ID.");
+		}
+
+
+		//return message.channel.send(messageID + "s content is: " + m.content)
+		if (!m.editable) return message.channel.send("I cannot edit that message.")
+		if (m.embeds.length < 1) return message.channel.send("This isn't an image submission.")
+		const url = m.embeds[0].image.url
+		const authorID = m.embeds[0].author.iconURL.match(/[0-9]{18}/gmi)
+		let title = null;
+		for (let field of m.embeds[0].fields) {
+			if (field.name == 'Title:') {
+				title = field.value;
+				break
+			}
+		}
+		if (title == null) {
+			return message.channel.send("This isn't an image submission.")
+		}
+		const author = await client.users.fetch(String(authorID));
+
+		//console.log(url, authorID, title, author.tag)
+		let exists = null;
+		for (let field of m.embeds[0].fields) {
+			if (field.name == 'Information:') {
+				exists = field.value;
+				break
+			}
+		}
+		//console.log(exists)
+		if (exists != null) {
+			return message.channel.send("This message has already been denied or approved.")
+		}
+		const newEmbed = new Discord.MessageEmbed()
+			.setAuthor(author.tag, author.avatarURL())
+			.setImage(url)
+			.setColor("#FFFF00")
+			.addField("Title:", "**" + title + "**")
+			.addField("Amazing picture by:", "<@" + message.author + ">")
+			.addField('\u200b', '\u200b')
+			.addField('Information:', ':white_check_mark: | Approved by Moderator: ' + reason);
+		m.edit(newEmbed)
+		await author.send("Your image submission was approved!: " + reason).then(() => {
+			message.channel.send("DM Sent.")
+		}).catch(() => {
+			message.channel.send("User has DMs closed / No mutual servers");
+		});
+	} else if (message.content.startsWith(prefix + "restore")) {
+		if (!botOwners.includes(message.author.id)) return// message.channel.send("Hmm... You don't seem to have enough permissions to do that.")
+		message.delete();
+		if (!args[0]) {
+			return message.channel.send("Please provide the message to restore")
+		}
+		const submissionChannel = client.channels.cache.get(submissionChannelID);
+		const messageID = args[0];
+		let m = await submissionChannel.messages.fetch(messageID).catch((err) => {
+			return message.channel.send("Invalid Message ID.")
+		})
+		if (m == undefined) {
+			return message.channel.send("Invalid Message ID.");
+		}
+
+
+		//return message.channel.send(messageID + "s content is: " + m.content)
+		if (!m.editable) return message.channel.send("I cannot edit that message.")
+		if (m.embeds.length < 1) return message.channel.send("This isn't an image submission.")
+		const url = m.embeds[0].image.url
+		const authorID = m.embeds[0].author.iconURL.match(/[0-9]{18}/gmi)
+		let title = null;
+		for (let field of m.embeds[0].fields) {
+			if (field.name == 'Title:') {
+				title = field.value;
+				break
+			}
+		}
+		if (title == null) {
+			return message.channel.send("This isn't an image submission.")
+		}
+		const author = await client.users.fetch(String(authorID));
+
+		//console.log(url, authorID, title, author.tag)
+		let exists = null;
+		for (let field of m.embeds[0].fields) {
+			if (field.name == 'Information:') {
+				exists = field.value;
+				break
+			}
+		}
+		//console.log(exists)
+		const newEmbed = new Discord.MessageEmbed()
+			.setAuthor(author.tag, author.avatarURL())
+			.setImage(url)
+			.setColor("#FFFF00")
+			.addField("Title:", "**" + title + "**")
+			.addField("Amazing picture by:", "<@" + message.author + ">")
+		m.edit(newEmbed).then((msg) => {
+			msg.reactions.removeAll()
+			msg.react("👍")
+			msg.react("👎")
+		}).catch(function (err) {
+			console.error("ERROR: " + err.message)
+		});
 	}
+
 })
+
+
 client.on('ready', () => {
 	console.log(client.user.tag + " is ready!")
 })
