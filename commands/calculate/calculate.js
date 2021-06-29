f37521974f5774a7f7ea8ea583440c731863e04c
ray <86501179+TheIIISociety@users.noreply.github.com>
2021-06-29 12:20:26 +0200

    Update calculate.js

--- commands/calculate/calculate.js
+++ commands/calculate/calculate.js
@@ -7,14 +7,15 @@ async function runCommand(message, args, RM) {
     const Discord = RM.Discord;
     const client = RM.client;
     const math = require("mathjs");
+    require("discord-reply")
 
-    if (!args[0]) return message.channel.send("**Enter Something To Calculate**");
+    if (!args[0]) return message.lineReply("**Enter Something To Calculate**");
 
     let result;
     try {
         result = math.evaluate(args.join(" ").replace(/[x]/gi, "*").replace(/[,]/g, ".").replace(/[÷]/gi, "/"));
     } catch (e) {
-        return message.channel.send("**Enter Valid Calculation!**\n\n**List of Calculations** - \n1. **sqrt equation** - `sqrt(3^2 + 4^2) = 5`\n2. **Units to Units** - `2 inch to cm = 0.58`\n3. **Complex Expressions Like** - `cos(45 deg) = 0.7071067811865476`\n4. **Basic Maths Expressions** - `+, -, ^, /, decimals` = **2.5 - 2 = 0.5**");
+        return message.lineReply("**Enter Valid Calculation!**\n\n**List of Calculations** - \n1. **sqrt equation** - `sqrt(3^2 + 4^2) = 5`\n2. **Units to Units** - `2 inch to cm = 0.58`\n3. **Complex Expressions Like** - `cos(45 deg) = 0.7071067811865476`\n4. **Basic Maths Expressions** - `+, -, ^, /, decimals` = **2.5 - 2 = 0.5**");
     }
 
     let embed = new Discord.MessageEmbed()
@@ -25,7 +26,7 @@ async function runCommand(message, args, RM) {
         .addField("**Operation**", `\`\`\`Js\n${args.join("").replace(/[x]/gi, "*").replace(/[,]/g, ".").replace(/[÷]/gi, "/")}\`\`\``)
         .addField("**Result**", `\`\`\`Js\n${result}\`\`\``)
         .setFooter(message.guild.name, message.guild.iconURL());
-    message.channel.send(embed);
+    message.lineReply(embed);
     console.log("calc used")
 
 }
