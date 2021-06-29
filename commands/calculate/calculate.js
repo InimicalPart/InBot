53aab1fab71c8ba5bb7ff402b9c56191d7656858
ray <86501179+TheIIISociety@users.noreply.github.com>
2021-06-29 11:45:51 +0200

    hi inimi

---
+++ commands/calculate/calculate.js
@@ -0,0 +1,46 @@
+const commandInfo = {
+    "possibleTriggers": ["calc", "math", "calculate"],
+    "help": "`.calculate: will calculate a math equation that you input eg. `.calc 2+2`\nAliases: .calc, .math"
+}
+
+async function runCommand(message, args, RM) {
+    const Discord = RM.Discord;
+    const client = RM.client;
+    const math = require("mathjs");
+
+    if (!args[0]) return message.channel.send("**Enter Something To Calculate**");
+
+    let result;
+    try {
+        result = math.evaluate(args.join(" ").replace(/[x]/gi, "*").replace(/[,]/g, ".").replace(/[÷]/gi, "/"));
+    } catch (e) {
+        return message.channel.send("**Enter Valid Calculation!**\n\n**List of Calculations** - \n1. **sqrt equation** - `sqrt(3^2 + 4^2) = 5`\n2. **Units to Units** - `2 inch to cm = 0.58`\n3. **Complex Expressions Like** - `cos(45 deg) = 0.7071067811865476`\n4. **Basic Maths Expressions** - `+, -, ^, /, decimals` = **2.5 - 2 = 0.5**");
+    }
+
+    let embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setAuthor(`${client.user.username} Calculator`, message.author.displayAvatarURL({
+            dynamic: true
+        }))
+        .addField("**Operation**", `\`\`\`Js\n${args.join("").replace(/[x]/gi, "*").replace(/[,]/g, ".").replace(/[÷]/gi, "/")}\`\`\``)
+        .addField("**Result**", `\`\`\`Js\n${result}\`\`\``)
+        .setFooter(message.guild.name, message.guild.iconURL());
+    message.channel.send(embed);
+    console.log("calc used")
+
+}
+
+function commandAlias() {
+    return commandInfo.possibleTriggers;
+}
+
+function commandHelp() {
+    return commandInfo.help;
+}
+module.exports = {
+    runCommand,
+    commandAlias,
+    commandHelp
+}
+
+console.log("[I] CALCULATE initialized [I]")
