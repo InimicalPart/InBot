dcf670c7ecf601109e2508eebf046eef5e434c66
InimicalPart <inimicalpart@gmail.com>
2022-02-21 07:45:40 +0100

    a

--- execute.js
+++ execute.js
@@ -1,1 +1,1 @@
-let child_process = require("child_process");child_process.exec('cd /coding/projects/iii.exe && git add . && git commit -m "hi" && git push')
+let child_process = require("child_process");child_process.exec('code-insiders')
